228e18491ce0d436b9e10cfc833680a8730d36ca
ankitOpash <ay.opash@gmail.com>
2025-04-18 11:13:39 +0530

    fix the file delete functionality

--- routes/api/admin/chats.routes.js
+++ routes/api/admin/chats.routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { getChatReports, getUnReadChatCounts } = require("../../../controllers/chat/chat.controller");
+const { getChatReports, getUnReadChatCounts, getChatTrends } = require("../../../controllers/chat/chat.controller");
 const router = express.Router();
 
 //chat-reports
 router.get("/reports", getChatReports);
 
 router.get("/unread-chats", getUnReadChatCounts);
+
+router.get("/chat-trends", getChatTrends);
+ 
 
 module.exports = router;
