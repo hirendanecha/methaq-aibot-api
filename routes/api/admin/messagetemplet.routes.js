28c3202f2701f76d3e301435053fffe1652f348e
ankitOpash <ay.opash@gmail.com>
2025-05-22 12:45:46 +0530

    whataasp templet delete

--- routes/api/admin/messagetemplet.routes.js
+++ routes/api/admin/messagetemplet.routes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const agentCtrl = require("../../../controllers/admin/agents.controller");
 const { permissionAuthorization } = require('../../../middleware/authorization');
 const { createTemplate, updateTemplate, deleteTemplate, incrementUsage, getAllTemplates,getTemplateById } = require('../../../controllers/message-templet/messageTemplate.controller');
-const { getAllWhatsappTemplet } = require('../../../controllers/whatassp-templet/whatasspTemplet.controller');
+const { getAllWhatsappTemplet, deleteWhatsappTemplet } = require('../../../controllers/whatassp-templet/whatasspTemplet.controller');
 
 /* APIs for agents
  1. Create message templet
@@ -20,6 +20,9 @@ router.post('/:id/incrementtemestempletuse',incrementUsage);
 
 //whatsapp templet
 router.get("/get-whatsapp-templet",getAllWhatsappTemplet);
+
+// Delete WhatsApp template by name
+router.delete('/delete-whatsapp-templet',permissionAuthorization("commonPermission.whatsappTemplet", ["delete"],["Admin"]), deleteWhatsappTemplet);
 
 // router.post("/crete-whatassp-messtemplet", permissionAuthorization("commonPermission.whatsappTemplet", ["create"],["Admin"]), createTemplate);
 
