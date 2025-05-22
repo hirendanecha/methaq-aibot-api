2edd7049e916cb9c21d3e9ecef8e1e772357bc56
Hrithik Jaiswal <hrj.opash@gmail.com>
2025-05-22 18:50:55 +0530

    create whatsapp template api

--- routes/api/admin/messagetemplet.routes.js
+++ routes/api/admin/messagetemplet.routes.js
@@ -2,27 +2,40 @@ var express = require('express');
 var router = express.Router();
 const agentCtrl = require("../../../controllers/admin/agents.controller");
 const { permissionAuthorization } = require('../../../middleware/authorization');
-const { createTemplate, updateTemplate, deleteTemplate, incrementUsage, getAllTemplates,getTemplateById } = require('../../../controllers/message-templet/messageTemplate.controller');
-const { getAllWhatsappTemplet, deleteWhatsappTemplet } = require('../../../controllers/whatassp-templet/whatasspTemplet.controller');
+const { createTemplate, updateTemplate, deleteTemplate, incrementUsage, getAllTemplates, getTemplateById } = require('../../../controllers/message-templet/messageTemplate.controller');
+const { getAllWhatsappTemplet, deleteWhatsappTemplet, createWhatsappTemplate } = require('../../../controllers/whatassp-templet/whatasspTemplet.controller');
+const pdfUpload = require("../../../middleware/file-upload");
+
 
 /* APIs for agents
  1. Create message templet
  2. Get  message templet
 
 */
-router.post("/cretemesstemplet", permissionAuthorization("commonPermission.templet", ["create"],["Admin"]), createTemplate);
-router.put('/updatetemestemplet/:id', permissionAuthorization("commonPermission.templet", ["update"],["Admin"]),updateTemplate);
-router.delete('/deletemestemplet/:id', permissionAuthorization("commonPermission.templet", ["delete"],["Admin"]),deleteTemplate);
-router.get("/gettemestemplets",getAllTemplates);
-router.get("/gettemestempletById/:id",getTemplateById);
+router.post("/cretemesstemplet", permissionAuthorization("commonPermission.templet", ["create"], ["Admin"]), createTemplate);
+router.put('/updatetemestemplet/:id', permissionAuthorization("commonPermission.templet", ["update"], ["Admin"]), updateTemplate);
+router.delete('/deletemestemplet/:id', permissionAuthorization("commonPermission.templet", ["delete"], ["Admin"]), deleteTemplate);
+router.get("/gettemestemplets", getAllTemplates);
+router.get("/gettemestempletById/:id", getTemplateById);
 
-router.post('/:id/incrementtemestempletuse',incrementUsage);
+router.post('/:id/incrementtemestempletuse', incrementUsage);
 
 //whatsapp templet
-router.get("/get-whatsapp-templet",getAllWhatsappTemplet);
+router.post("/create-whatsapp-templet", permissionAuthorization("commonPermission.whatsappTemplet", ["create"], ["Admin"]), pdfUpload.fileUpload(
+    "templateDoc",
+    ["pdf", "image", "video"],
+    [
+        {
+            name: "doc",
+            maxCount: 1,
+        },
+    ]
+), createWhatsappTemplate);
+
+router.get("/get-whatsapp-templet", permissionAuthorization("commonPermission.whatsappTemplet", ["read"], ["Admin"]), getAllWhatsappTemplet);
 
 // Delete WhatsApp template by name
-router.delete('/delete-whatsapp-templet',permissionAuthorization("commonPermission.whatsappTemplet", ["delete"],["Admin"]), deleteWhatsappTemplet);
+router.delete('/delete-whatsapp-templet', permissionAuthorization("commonPermission.whatsappTemplet", ["delete"], ["Admin"]), deleteWhatsappTemplet);
 
 // router.post("/crete-whatassp-messtemplet", permissionAuthorization("commonPermission.whatsappTemplet", ["create"],["Admin"]), createTemplate);
 
